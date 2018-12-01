ec45f08909c8db047932d9046ed84125da32926c
prithviramesh <prithviramesh93@gmail.com>
2018-12-01 15:21:39 -0800

    playing around with RootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,6 @@ var RootCmd = &cobra.Command {
 	Short: "finds free TCP ports available on your machine",
 	Long: "portfinder uses Go's built in net library to find free available TCP ports on localhost",
 	Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("what do I put here?")
+        fmt.Println(cmd)
     },
 }
