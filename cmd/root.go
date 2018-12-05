816f9dba898a49c4ae4b339d5598fb07706704c9
prithviramesh <prithviramesh93@gmail.com>
2018-12-04 17:30:37 -0800

    fixed string construction for Example field

--- cmd/root.go
+++ cmd/root.go
@@ -9,8 +9,7 @@ import (
 var RootCmd = &cobra.Command {
 	Use: "free-port-finder",
 	Short: "free-port-finder uses Go's built in net library to find free available TCP ports on localhost\n",
-	Example: "free-port-finder check <port-number> returns if the passed port is free\n
-	free-port-finder find returns a free port number if available\n"
+	Example: "free-port-finder check <port-number> returns if the passed port is free\nfree-port-finder find returns a free port number if available\n",
 	Run: func(cmd *cobra.Command, args []string) {
         fmt.Printf(cmd.Example)
     },
